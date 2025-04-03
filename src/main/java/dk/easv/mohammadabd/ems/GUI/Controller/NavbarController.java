c27c00aad4f55060bf25cb5a6f4dc65953f03bcb
MohammadAbd2 <moha.abd.99@gmail.com>
2025-04-03 04:43:28 +0200

    Switching between Scene added

--- src/main/java/dk/easv/mohammadabd/ems/GUI/Controller/NavbarController.java
+++ src/main/java/dk/easv/mohammadabd/ems/GUI/Controller/NavbarController.java
@@ -1,10 +1,20 @@
 package dk.easv.mohammadabd.ems.GUI.Controller;
 
+import dk.easv.mohammadabd.ems.GUI.View.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class NavbarController {
     @FXML
@@ -16,10 +26,206 @@ public class NavbarController {
         profile_pic.setImage(image);
 
         // Apply circular clipping
-        Circle clip = new Circle(profile_pic.getFitWidth() / 3, profile_pic.getFitHeight() / 3, Math.min(profile_pic.getFitWidth(), profile_pic.getFitHeight()) / 3);
+        Circle clip = new Circle(profile_pic.getFitWidth() / 3, profile_pic.getFitHeight() / 3,
+                Math.min(profile_pic.getFitWidth(), profile_pic.getFitHeight()) / 3);
         profile_pic.setClip(clip);
     }
-    public void ticketTab(ActionEvent event) {
 
+    public void ticketTab(ActionEvent event) {
+        try {
+            // declaration of the variables
+            VBox Application = new VBox();
+            VBox WindowBox = new VBox();
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            VBox rootContainer = new VBox();
+            VBox Body  = new VBox();
+            // Random Component to add to the Tab pages
+            CustomTitleBar customTitleBar = new CustomTitleBar(currentStage);
+
+            // sort VBoxes
+            Application.getChildren().add(WindowBox);
+            Application.getChildren().add(rootContainer);
+            WindowBox.getChildren().add(customTitleBar);
+            rootContainer.getChildren().add(Navbar.loadNavbar());
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+            // Add the ScrollPane
+            ScrollPane scrollPane = new ScrollPane();
+            scrollPane.setContent(Body);
+            scrollPane.setFitToWidth(true);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollPane.getStyleClass().add("scroll-pane");
+
+            rootContainer.getChildren().add(scrollPane);
+            // set the new Scene to the Stage
+            Scene currentScene = currentStage.getScene();
+            currentScene.setRoot(Application);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void HomeTab(ActionEvent event) {
+        try {
+            // declaration of the variables
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            VBox Application = new VBox();
+            VBox WindowBox = new VBox();
+            VBox rootContainer = new VBox();
+            VBox Body  = new VBox();
+
+            // Sort the VBox to fit with the style and Application
+            CustomTitleBar customTitleBar = new CustomTitleBar(currentStage);
+            WindowBox.getChildren().add(customTitleBar);
+            Application.getChildren().add(WindowBox);
+            rootContainer.getChildren().add(Navbar.loadNavbar());
+
+            //add the new component here bellow
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+
+
+            // create and add the ScrollPane
+            ScrollPane scrollPane = new ScrollPane();
+            scrollPane.setContent(Body);
+            scrollPane.setFitToWidth(true);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollPane.getStyleClass().add("scroll-pane");
+            rootContainer.getChildren().add(scrollPane);
+
+            //render & set the new Scene to the Stage
+            Application.getChildren().add(rootContainer);
+            Scene currentScene = currentStage.getScene();
+            currentScene.setRoot(Application);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void EventTab(ActionEvent event) {
+        try {
+            // declaration of the variables
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            VBox Application = new VBox();
+            VBox WindowBox = new VBox();
+            VBox rootContainer = new VBox();
+            VBox Body  = new VBox();
+
+            // Sort the VBox to fit with the style and Application
+            CustomTitleBar customTitleBar = new CustomTitleBar(currentStage);
+            WindowBox.getChildren().add(customTitleBar);
+            Application.getChildren().add(WindowBox);
+            rootContainer.getChildren().add(Navbar.loadNavbar());
+
+            //add the new component here bellow
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+
+
+            // create and add the ScrollPane
+            ScrollPane scrollPane = new ScrollPane();
+            scrollPane.setContent(Body);
+            scrollPane.setFitToWidth(true);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollPane.getStyleClass().add("scroll-pane");
+            rootContainer.getChildren().add(scrollPane);
+
+            //render & set the new Scene to the Stage
+            Application.getChildren().add(rootContainer);
+            Scene currentScene = currentStage.getScene();
+            currentScene.setRoot(Application);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void CoordinatorTab(ActionEvent event) {
+        try {
+            // declaration of the variables
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            VBox Application = new VBox();
+            VBox WindowBox = new VBox();
+            VBox rootContainer = new VBox();
+            VBox Body  = new VBox();
+
+            // Sort the VBox to fit with the style and Application
+            CustomTitleBar customTitleBar = new CustomTitleBar(currentStage);
+            WindowBox.getChildren().add(customTitleBar);
+            Application.getChildren().add(WindowBox);
+            rootContainer.getChildren().add(Navbar.loadNavbar());
+
+            //add the new component here bellow
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+
+
+            // create and add the ScrollPane
+            ScrollPane scrollPane = new ScrollPane();
+            scrollPane.setContent(Body);
+            scrollPane.setFitToWidth(true);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollPane.getStyleClass().add("scroll-pane");
+            rootContainer.getChildren().add(scrollPane);
+
+            //render & set the new Scene to the Stage
+            Application.getChildren().add(rootContainer);
+            Scene currentScene = currentStage.getScene();
+            currentScene.setRoot(Application);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ProfileTab(ActionEvent event) {
+        try {
+            // declaration of the variables
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            VBox Application = new VBox();
+            VBox WindowBox = new VBox();
+            VBox rootContainer = new VBox();
+            VBox Body  = new VBox();
+
+            // Sort the VBox to fit with the style and Application
+            CustomTitleBar customTitleBar = new CustomTitleBar(currentStage);
+            WindowBox.getChildren().add(customTitleBar);
+            Application.getChildren().add(WindowBox);
+            rootContainer.getChildren().add(Navbar.loadNavbar());
+
+            //add the new component here bellow
+            Body.getChildren().add(Slider.loadSlider());
+            Body.getChildren().add(Slider.loadSlider());
+
+
+            // create and add the ScrollPane
+            ScrollPane scrollPane = new ScrollPane();
+            scrollPane.setContent(Body);
+            scrollPane.setFitToWidth(true);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollPane.getStyleClass().add("scroll-pane");
+            rootContainer.getChildren().add(scrollPane);
+
+            //render & set the new Scene to the Stage
+            Application.getChildren().add(rootContainer);
+            Scene currentScene = currentStage.getScene();
+            currentScene.setRoot(Application);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/dk/easv/mohammadabd/ems/GUI/View/Login.java
+++ src/main/java/dk/easv/mohammadabd/ems/GUI/View/Login.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
-public class login {
+public class Login {
     @FXML
     private Button ticketButton; // Button to trigger the ticketTab method
 
