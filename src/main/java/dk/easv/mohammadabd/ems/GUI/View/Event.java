63d6edbb00ca51f62b8500cab8d1fc5bce494112
Asparrow13 <145340503+Asparrow13@users.noreply.github.com>
2025-04-10 17:51:05 +0200

    new files added

---
+++ src/main/java/dk/easv/mohammadabd/ems/GUI/View/Event.java
@@ -0,0 +1,34 @@
+package dk.easv.mohammadabd.ems.GUI.View;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class Event {
+    public static Node loadPage(){
+        try {
+            VBox application = new VBox();
+            VBox rootContainer = new VBox();
+            VBox body = new VBox();
+
+            FXMLLoader loader = new FXMLLoader(TicketPage.class.getResource("/dk/easv/mohammadabd/ems/EventPageBruno.fxml"));
+            Parent ticketContent = loader.load();
+
+            body.getChildren().add(ticketContent);
+
+            rootContainer.getChildren().add(body);
+            application.getChildren().add(rootContainer);
+
+            return application;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
